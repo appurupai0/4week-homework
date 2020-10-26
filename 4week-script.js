1377e471eea8daeb97469a4409eed50c48a1e163
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2020-10-26 16:04:13 +0900

    buttonで機能できるようにした

--- 4week-script.js
+++ 4week-script.js
@@ -2,33 +2,34 @@
 // 問題文はindex.htmlにあります。
 
 
-// Q 1
-let message = document.getElementById('q1-text');
-console.log(message);
+	function btn(){
+		// Q 1
+		let message = document.getElementById('q1-text');
+		console.log(message);
 
-// Q 2
-let moji = document.getElementById('q2-text');
-console.log(moji);
-moji.textContent = "この文字に変更されたよ"
+		// Q 2
+		let moji = document.getElementById('q2-text');
+		console.log(moji);
+		moji.textContent = "この文字に変更されたよ"
 
-// Q 3
-let textcolor = document.getElementById('q3-text');
-console.log(textcolor);
-// 上のconsole.log()は文字や色の変更の時にはわざわざ書かなくてもよい
-textcolor.style.color = 'blue';
+		// Q 3
+		let textcolor = document.getElementById('q3-text');
+		console.log(textcolor);
+		// 上のconsole.log()は文字や色の変更の時にはわざわざ書かなくてもよい
+		textcolor.style.color = 'blue';
 
-// Q 4 (難)
-// 「for of」を使います。
-let change = document.getElementsByClassName('q4-text');
+		// Q 4 (難)
+		// 「for of」を使います。
+		let change = document.getElementsByClassName('q4-text');
 
-for(let num of change){
-	console.log(change[0]);
-	// change[0].style.color = 'blue'
-	num.style.color = 'red'
-}
-// Q 5
-let background = document.getElementsByClassName('q5-text');
-for(let number of background){
-	number.style.backgroundColor = 'pink';
-}
-
+		for(let num of change){
+			console.log(change[0]);
+			// change[0].style.color = 'blue'
+			num.style.color = 'red'
+		}
+		// Q 5
+		let background = document.getElementsByClassName('q5-text');
+		for(let number of background){
+			number.style.backgroundColor = 'pink';
+		}
+	}
